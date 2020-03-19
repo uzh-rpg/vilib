cae4bf29cd22a35da4491f334f1cc2cfd79d8492
Balazs Nagy <balimobil@gmail.com>
2020-03-19 12:11:33 +0100

    Whitespace fix for the CC utility

--- visual_lib/utility/determine_cc.cpp
+++ visual_lib/utility/determine_cc.cpp
@@ -7,42 +7,42 @@
 #include <string>
 #include <cuda_runtime_api.h>
 
-#define CUDA_API_CALL(__CALL__)  do {                                          \
-                                  const cudaError_t a = __CALL__;              \
-                                  if (a != cudaSuccess) {                      \
-                                    std::cout << "CUDA Error: " << 			   \
-										cudaGetErrorString(a)   <<             \
-										"(err_num=" << a << ")" << std::endl;  \
-                                    std::cout << "File: " << __FILE__ <<       \
-										" | Line: " << __LINE__ << std::endl;  \
-                                    cudaDeviceReset();                         \
-                                    return -1;								   \
-								  }											   \
-								} while(0)
+#define CUDA_API_CALL(__CALL__)  do {                                           \
+                                   const cudaError_t a = __CALL__;              \
+                                   if (a != cudaSuccess) {                      \
+                                     std::cout << "CUDA Error: " <<             \
+                                      cudaGetErrorString(a)   <<                \
+                                      "(err_num=" << a << ")" << std::endl;     \
+                                     std::cout << "File: " << __FILE__ <<       \
+                                       " | Line: " << __LINE__ << std::endl;    \
+                                     cudaDeviceReset();                         \
+                                     return -1;                                 \
+                                   }                                            \
+                                 } while(0)
 
 int main(int argc, char * argv[]) {
-	int device_count = 0;
-	CUDA_API_CALL(cudaGetDeviceCount(&device_count));
-	if(device_count == 0) {
-		std::cout << "No CUDA-capable device was found" << std::endl;
-		return -1;
-	}
+  int device_count = 0;
+  CUDA_API_CALL(cudaGetDeviceCount(&device_count));
+  if(device_count == 0) {
+    std::cout << "No CUDA-capable device was found" << std::endl;
+    return -1;
+  }
 
-	std::string nvcc_arg;
-	for(int i=0;i<device_count;++i) {
-		cudaSetDevice(i);
-		cudaDeviceProp deviceProp;
-    	cudaGetDeviceProperties(&deviceProp, i);
-		if(nvcc_arg.length()) {
-          nvcc_arg += " ";
-		}
-		nvcc_arg += "--generate-code arch=compute_" 
-				 + std::to_string(deviceProp.major)
-				 + std::to_string(deviceProp.minor)
-				 + ",code=sm_"
-				 + std::to_string(deviceProp.major)
-				 + std::to_string(deviceProp.minor);
-	}
-    std::cout << nvcc_arg << std::endl;
-	return 0;
+  std::string nvcc_arg;
+  for(int i=0;i<device_count;++i) {
+    cudaSetDevice(i);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, i);
+    if(nvcc_arg.length()) {
+      nvcc_arg += " ";
+    }
+    nvcc_arg += "--generate-code arch=compute_"
+             + std::to_string(deviceProp.major)
+             + std::to_string(deviceProp.minor)
+             + ",code=sm_"
+             + std::to_string(deviceProp.major)
+             + std::to_string(deviceProp.minor);
+  }
+  std::cout << nvcc_arg << std::endl;
+  return 0;
 }
